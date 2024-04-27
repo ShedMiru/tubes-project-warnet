452853e885c05b994bae11cb2ef3f5499edd43cf
Star17845 <165618013+Star17845@users.noreply.github.com>
2024-04-27 15:21:08 +0700

    Update warnet.c
    add regis and login pelanggan

--- warnet.c
+++ warnet.c
@@ -15,10 +15,17 @@ int i, choice;
 
 void Pelanggan();
 // void Billing();
-// void LoginPelanggan();
+void LoginPelanggan();
+void regispelanggan();
+void menupelanggan();
 
 int OP(int);
 void MenuOP();
+
+struct{
+    char username[100];
+    char pass[100];
+}p;
 
 void main()
 {
@@ -50,6 +57,8 @@ void main()
 
 void Pelanggan()
 {
+    system("cls");
+    int choice2;
     printf("*UI*\n");
     printf("1. Beli Billing\n");
     printf("2. Login\n");
@@ -61,7 +70,23 @@ void Pelanggan()
         // Billing();
         break;
     case 2:
-        // LoginPelanggan();
+        printf("1. Login\n");
+        printf("2. Register\n");
+        printf("Pilih: ");
+        scanf("%d",&choice2);
+
+        switch(choice2){
+            case 1:
+            LoginPelanggan();
+            break;
+            case 2:
+            regispelanggan();
+            break;
+            default:
+            printf("Pilihan tidak tersedia");
+            system("pause");
+            Pelanggan();
+        }
         break;
     default:
         printf("Pilihan tidak ditemukan, kembali ke Menu\n");
@@ -69,6 +94,68 @@ void Pelanggan()
         main();
         break;
     }
+}
+void regispelanggan(){
+    char username[100];
+    FILE *fp=fopen("datapelanggan.dat","ab");
+    FILE *fr=fopen("datapelanggan.dat","rb");
+    lup:
+    system("cls");
+    fflush(stdin);
+    printf("=======REGISTER=======\n");
+    printf("Username: ");
+    gets(username);
+    while(fread(&p, sizeof(p),1,fr)){
+        if(strcmp(username,p.username)==0){
+            printf("Username telah dipakai, silahkan pilih username lain\n");
+            system("pause");
+            goto lup;
+        }
+    }
+    strcpy(p.username,username);
+    printf("Password: ");
+    gets(p.pass);
+    fwrite(&p, sizeof(p), 1, fp);
+    fclose(fp);
+    fclose(fr);
+    printf("Registrasi Berhasil!!\n");
+    system("pause");
+    Pelanggan();
+}
+
+void LoginPelanggan(){
+    char user[100],pass[100];
+    int i;
+    for(i=3;i>0;i--){
+    fflush(stdin);
+    system("cls");
+    printf("=======LOGIN=======\n");
+    printf("Username: ");
+    gets(user);
+    printf("Password: ");
+    gets(pass);
+    if(strcmp(user,p.username)==0 && strcmp(pass,p.pass)==0){
+        printf("LOGIN BERHASIL!!\n");
+        i=0;
+        system("pause");
+        menupelanggan();
+    }
+    else{
+        if(i>1){
+        printf("Username atau password salah, kesempatan sisa %d silahkan coba lagi\n",i-1);
+        system("pause");
+        }
+        else{
+            printf("Kesempatan habis!!\n");
+            system("pause");
+            main();
+        }
+    }
+    }
+}
+
+void menupelanggan(){
+
 }
 
 int OP(int n)
